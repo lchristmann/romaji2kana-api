ed6aacf355c5e51e5bdb9197c658cf56a4169997
Leander Christmann <leander.christmann@proton.me>
2023-09-16 23:56:26 +0200

    build the API + deploy scripts

--- index.js
+++ index.js
@@ -1,57 +1,39 @@
-import express from "express";
 import { isJapanese, isKana, isHiragana, isKatakana, isRomaji, isMixed, toKana, toHiragana, toKatakana, toRomaji } from 'wanakana';
 
-const app = express();
-const PORT = 3000;
-
-app.get('/', (req, res) => {
-    res.send('Hello world!');
-});
-
-const endpoints = ['/v1/is/japanese', '/v1/is/kana', '/v1/is/hiragana', '/v1/is/katakana', '/v1/is/romaji', '/v1/is/mixed',
-    '/v1/to/kana', '/v1/to/hiragana', '/v1/to/katakana', '/v1/to/romaji'];
-
-app.get(endpoints, (req, res) => {
-    // pack path and query params into an event object structure like the Lambda function receives
-    // so ideally there's as little difference as possible between this local development API version and the real one
-    const event = {
-        path: req.url.split('?')[0],
-        queryStringParameters: req.query.q
-    };
-    res.send(handler(event));
-});
-
-// this function simulates the handler() function from the Lambda (event structure same, regarding those 2 params)
-function handler(event) {
-    console.log(`---------- ${event.path} ----------`);
-    console.log(`q: ${event.queryStringParameters}`);
-
-    // cut off the "/v1/" from the URL and evaluate which operation to execute
-    switch ((event.path).substring(4)) {
+export const handler = async (event) => {
+    // event.resource is e.g. "/v1/is/hiragana" => cut the leading "/v1/"
+    switch (event.resource.substring(4)) {
         case "is/japanese":
-            return {result: isJapanese(event.queryStringParameters)};
+            return formatResponse({a: isJapanese(event.queryStringParameters.q)});
         case "is/kana":
-            return {result: isKana(event.queryStringParameters)};
+            return formatResponse({a: isKana(event.queryStringParameters.q)});
         case "is/hiragana":
-            return {result: isHiragana(event.queryStringParameters)};
+            return formatResponse({a: isHiragana(event.queryStringParameters.q)});
         case "is/katakana":
-            return {result: isKatakana(event.queryStringParameters)};
+            return formatResponse({a: isKatakana(event.queryStringParameters.q)});
         case "is/romaji":
-            return {result: isRomaji(event.queryStringParameters)};
+            return formatResponse({a: isRomaji(event.queryStringParameters.q)});
         case "is/mixed":
-            return {result: isMixed(event.queryStringParameters)};
+            return formatResponse({a: isMixed(event.queryStringParameters.q)});
         case "to/kana":
-            return {result: toKana(event.queryStringParameters)};
+            return formatResponse({a: toKana(event.queryStringParameters.q)});
         case "to/hiragana":
-            return {result: toHiragana(event.queryStringParameters)};
+            return formatResponse({a: toHiragana(event.queryStringParameters.q)});
         case "to/katakana":
-            return {result: toKatakana(event.queryStringParameters)};
+            return formatResponse({a: toKatakana(event.queryStringParameters.q)});
         case "to/romaji":
-            return {result: toRomaji(event.queryStringParameters)};
+            return formatResponse({a: toRomaji(event.queryStringParameters.q)});
     }
 }
 
-// Running the app on the server
-app.listen(PORT, () => {
-    console.log(`App is running on port ${PORT}`);
-});
+// format the response for API Gateway in the Lambda function response format 1.0
+function formatResponse(body) {
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "isBase64Encoded": false,
+        "body": JSON.stringify(body)
+    }
+}
